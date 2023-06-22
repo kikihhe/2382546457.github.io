1dbac21ca773f60ad3dc9e7ece47f89786fb6fe5
2382546457 <2382546457@qq.com>
2023-06-22 14:29:37 +0800

    开发规范

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -112,7 +112,7 @@ module.exports = {
       ]
     },
     footer: { // 页脚信息
-      createYear: 2019, // 博客创建年份
+      createYear: 2023, // 博客创建年份
       copyrightInfo: '何 | MIT License', // 博客版权信息，支持a标签
     },
     htmlModules,
